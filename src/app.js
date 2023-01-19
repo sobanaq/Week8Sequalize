110a99fe9a48dbf6874a66e6e998847885e35aca
Sobana Qadus <sobanaq@hotmail.com>
2023-01-19 16:04:59 +0000

    Sequalize database with comments

--- src/app.js
+++ src/app.js
@@ -20,19 +20,30 @@ async function app(yargsInput) {
     });
     //------------------------------------------------------------------------------------------------------
   } else if (yargsInput.read) {
+    //place code to list all our movies here
     const results = await Movie.findAll({});
     // console.log(results);
     const newArray = [];
-    for (let index = 0; index < results.length; index++) {
-      const element = results[index];
-      const title = results[index].title;
-      const actor = results[index].actor;
-      const director = results[index].director;
-      newArray.push({ title: title, actor: actor, director: director });
-      // console.log(title, actor, director);
-    }
-    console.table(newArray);
-    //place code to list all our movies here
+    //Firstly, you need to create a for loop, type for and in VSC list of options select second 'for' with the box icon.
+    //This brings up a for loop with 'let index'.
+    //Replace the word array in the statement (appear twice) with result as we want a list element of results, 
+    //This is an ARRAY represented by [].
+    //We then want to find some fields and list them. we therefore const 'FIELD' = results[index].'FIELD'
+    //try the command 'node src/app.js --read'. This will list everthing in those fields. We want it neat in a table now.
+    //Secondly,we need to create a new array for the results to be put into a table. SEE BELOW.
+  }
+  for (let index = 0; index < results.length; index++) {
+    const element = results[index];
+    const title = results[index].title;
+    const actor = results[index].actor;
+    const director = results[index].director;
+    newArray.push({ title: title, actor: actor, director: director });
+    //This is a new array where we are saying find title, actor and director and push this data
+    //onto the new array. Then we use console.table(newArray-this is what we called the new array see line 25).
+    //The data pushed onto the newArray now displays as a neat table.
+    // console.log(title, actor, director);
+  }
+  console.table(newArray);
     //------------------------------------------------------------------------------------------------------
   } else if (yargsInput.updateActor) {
     //place code to update actor field here
@@ -75,6 +86,5 @@ async function app(yargsInput) {
   } else {
     console.log("Unrecognized Yargs command");
   }
-}
 
 app(yargs.argv);
